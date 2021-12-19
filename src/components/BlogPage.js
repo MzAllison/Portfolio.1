4643302a2892b80467918a1f0f276d785d9ca0bc
MzAllison <isituaho@gmail.com>
2021-12-19 11:28:58 +0100

    blog post

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
+import  LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
+import PowerButton from '../subComponents/PowerButton'
+
+import {Blogs} from '../data/BlogData';
+import BlogComponent from './BlogComponent'
 
 
 const MainContainer = styled.div`
 background-image: url(${img});
-background-size:cover;
+background-size: cover;
 background-repeat: no-repeat;
 background-attachment: fixed;
 background-position:  center;
-width: 100vw;
-height:100vh;
 `
 const Container = styled.div`
 background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`} ;
 width: 100%;
-height: auto;
+height:auto;
+
 position:relative;
 padding-bottom: 5rem;
 
 `
+
+const Center = styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
+padding-top: 10rem;
+`
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+grid-gap: calc(1rem + 2vw);
+`
+
+
 
 
 
@@ -29,7 +48,16 @@ const BlogPage = () => {
             <Container>
             <LogoComponent />
             <PowerButton />
-
+            <SocialIcons />
+<Center>
+<Grid>
+    {
+        Blogs.map(blog => {
+           return <BlogComponent  key={blog.id} blog={blog} />
+        })
+    }
+</Grid>
+</Center>
             </Container>
         </MainContainer>
     )
