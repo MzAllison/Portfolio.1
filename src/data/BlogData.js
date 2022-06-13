b855463b284756980ff5813ef0f2ea8332f853e8
MzAllison <isituaho@gmail.com>
2022-06-13 12:50:18 +0100

    new blog post

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,41 +1,41 @@
 export const Blogs = [
     {
         id:1,
-        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
-        tags:["react","gsap","styled-components"],
-        date:"13 May, 2021",
+        name:"Build Website with Html, Css and Javascript",
+        tags:["html","Css","Javascript"],
+        date:"17 April, 2022",
         imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
+        link:"https://dev.to/allison_m_08b9005267b3bcf/baby-steps-into-tech-3790"
     },
     {
         id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
+        name:"How to create an influencer website using J-query",
+        tags:["Html/Css","j-query"],
+        date:"15 May, 2022",
         imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+        link:"https://dev.to/allison_m_08b9005267b3bcf/tech-blogger-57ak"
     },
     {
         id:3,
         name:"React Loading Screen: Try these 3 cool loading screens for your app",
-        tags:["react","react-lottie","styled-components"],
-        date:"6 July, 2021",
+        tags:["Html/Css","flex-box"],
+        date:"15 May, 2022",
         imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://codebucks.hashnode.dev/react-loading-screens"
+        link:"https://dev.to/allison_m_08b9005267b3bcf/flex-box-2jck"
     },
     {
         id:4,
-        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+        name:"How to build a Fabulous Calculator using Javascript",
         tags:["react","redux","framer-motion"],
-        date:"26 May, 2021",
+        date:"13 June, 2022",
         imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
-        link:"https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1"
+        link:"https://dev.to/allison_m_08b9005267b3bcf/building-a-fancy-calculator-with-javascript-5c61"
     },
     {
         id:5,
         name:"What is Higher Order Component (HOC) in React JS?",
         tags:["react","reactJS","web-development"],
-        date:"3 Feb, 2021",
+        date:"3 Feb, 2022",
         imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
         link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
     },
